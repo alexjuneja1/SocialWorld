57383a9f60b16fbc8d6f65ecfb68a26bf804213f
Emma Gilhuly <emmagilhuly@me.com>
2015-11-20 15:17:15 -0800

    passport local login and facebook login

--- models/user.js
+++ models/user.js
@@ -8,11 +8,26 @@ var userSchema = new Schema({
     username: String,
     email: String,
     password: String
+  },
+  facebook: {
+    id: String,
+    name: String,
+    token: String,
+    email: String
   }
+
 })
 
 userSchema.methods.info = function(){
   console.log('my username is ' + username)
+}
+
+userSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
+
+userSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.local.password)
 }
 
 var User = mongoose.model('User', userSchema)
